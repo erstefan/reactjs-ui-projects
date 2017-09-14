3c2de1ed72dc5baaa627f584c697a476abc74129
Stefan Rusu <ercosminstefan@gmail.com>
2017-09-14 10:48:42 +0100

    Styling

--- product-tile/src/App.js
+++ product-tile/src/App.js
@@ -1,57 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import styled from 'styled-components';
 import productImageSrc from './product-image.png';
 
+// Variables
+const $fontFamily = 'Montserrat';
+
 const ProductWrapper = styled.div`
     max-width: 700px;
-    margin: 90px auto 50px;
-    padding: 25px;
+    margin: 200px auto 0;
+    padding: 0 25px 25px;
     display: flex;
+    height: 220px;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
     justify-content: center;
     align-items: center;
+    background:#fff;
 `;
 
 const ProductContent = styled.div`
     text-align: left;
-    color: #fff;
+    color: #222;
+    margin-top: 30px;
     
     h3 {
-        font-family: "Montserrat";
+        font-family: $fontFamily;
         font-size: 40px;
-        margin-bottom: 10px;
+        margin-bottom: 20px;
+        margin-top: 0px;
     }
     
     p {
-        font-family: "Montserrat";
+        font-family: $fontFamily;
         font-size: 12px;
+        margin-top: 0;
+    }
+    
+    span {
+     font-family: $fontFamily;
+     font-size: 12px;
+     color: #8DE6D7;
+     text-transform: uppercase;
+     font-weight: 700;
+     letter-spacing: 2px;
+     margin-bottom: 15px;
     }
 `;
 
+const App = () => {
+    return <ProductTile />
 
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ProductTile />
-      </div>
-    );
-  }
-}
+};
 
 const ProductTile = () => {
     return (
-        <ProductWrapper>
-            <img src={productImageSrc} className="product-image" alt=""/>
-            <ProductContent>
-                <h3>iPhone X</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at debitis deleniti dignissimos hic iste
-                    neque nobis.</p>
-                <a href="#" className="buy-btn">Buy now</a>
-            </ProductContent>
-        </ProductWrapper>
+        <div className="product-tile-container">
+            <ProductWrapper>
+                <img src={productImageSrc} className="product-image" alt=""/>
+                <ProductContent>
+                    <span>5.8-inch display</span>
+                    <h3>iPhone X</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <br/>
+                    <div className="pricing-model-wrapper">
+                        <PricingModel model="From £47.95/mo. for up to 24 mo."/>
+                        <PricingModel model="Or pay now £999"/>
+                    </div>
+                </ProductContent>
+            </ProductWrapper>
+
+            <a href="#" className="preorder-btn">Preorder</a>
+        </div>
+    );
+};
+
+const PricingModel = (props) => {
+    const { model } = props;
+    return (
+        <div className="pricing-model-item">{model}</div>
     );
 };
 

644fb42a927234f0ee44c705337d00f661709f0f
Stefan Rusu <ercosminstefan@gmail.com>
2017-09-14 11:28:41 +0100

    Github Profile Card Initial commit

---
+++ github-profile-card/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './App.css';
+import GithubProfileCard from "./components/GithubProfileCard";
+
+const App = () => {
+  return <GithubProfileCard />;
+}
+
+export default App;
---
+++ github-profile-card/src/components/GithubProfileCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const GithubProfileCard = () => {
+
+    return (
+        <div>GithubProfileCard</div>
+    );
+};
+
+export default GithubProfileCard;
